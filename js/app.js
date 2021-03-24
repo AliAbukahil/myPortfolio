8aae5d69900f02cf619103168b5ccc9447292009
ali.abukahil <ali.abukahil@gmail.com>
2021-03-24 14:33:20 +0100

    nav bar + slide right side nav bar with Animation Done

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
 const slideNav = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll("nav-links li");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
   burger.addEventListener("click", () => {
     // The Toggle Nav.
     nav.classList.toggle("nav-active");
 
-    // the Animation section.
+    // the Animation section for the Links.
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = ``;
       } else {
         link.style.animation = `navLinkFade 0.5 ease forwards ${
-          index / 7 + 0.3
+          index / 7 + 4.1
         }s `;
       }
     });
+    // the Animation section for the Burger.
+    burger.classList.toggle("spin");
   });
 };
 
